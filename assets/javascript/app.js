3bb7401b5778c1ebb30e8bc301856f607a731d07
danman15726 <danman15726@gmail.com>
2019-11-03 18:36:36 -0600

    Successfully appended data to firebase regarding player 1 and player 2. This is done by clicking the join button. The idea now is, whenever someone closes the window, the game removes the player profile from firebase. That way, anyone can play anywhere in a more seemless manner.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,37 +9,43 @@ var firebaseConfig = {
     measurementId: "G-Z05PC3GZHP"
 };
 // Initialize Firebase
-
 firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
-database = firebase.database();
 
 
 const player1 = 
 {   
+    
     Name: "",
     Choice: "",
     Win: 0,
     Loss: 0,
     Tie: 0,
     Connection: false
-
-}
+};
 
 const player2 = 
 {   
+    
     Name: "",
     Choice: "",
     Win: 0,
     Loss: 0,
     Tie: 0,
     Connection: false
-}
+};
+
+let Number = 0;
+
+
+
+
 
 
 //responsible for deciding players 1 and 2;
 let array = [];
-// let counter = 0;
+let counter = 0;
 
 
 
@@ -50,12 +56,20 @@ const game = {
         console.log("Start function started");
         $(".container").text("Standing by for 2 connections");
 
-        if (array.length === 0){
-            array.push(player1);
-        }
-        else if (array.length === 1){
-            array.push(player2);
-        }
+        // if (array.length === 0){
+        //     array.push(player1);
+
+
+        // }
+        // else if (array.length === 1){
+        //     array.push(player2);
+
+
+        // }
+
+
+
+
 
 
         {
@@ -67,10 +81,72 @@ const game = {
         }   
         
 
-        return counter++;
+    
     },
     
 }
 
 game.start();
-console.log(counter);
+
+
+
+const playersDiv = database.ref('/playersDiv');
+const chatRef = database.ref('/chat');
+
+const connectedRef = database.ref('.info/connected');
+
+
+  
+//counter = 0
+  $(document).on("click", "#Join", function(){
+      
+      console.log("Join Button Activated");
+
+      if ( player1.Connection === false || player2.Connection === false){
+        console.log(`Player 1 is now ${player1.Connection}`);
+        console.log(`Player 2 is now ${player2.Connection}`);
+
+        number = (player1.Connection) ? 2:1 ;
+
+        const playerFile = database.ref("/playersDiv/" + number);
+        console.log(playerFile);
+
+        //This sets the value in Firebase
+        playerFile.set({
+            name: `Player ${number}`
+        });
+        
+
+
+        //This changes Connection Values to True accordingly
+        if(number === 1){
+            player1.Connection = true;
+        }
+        else {
+            player2.Connection = true;
+        }
+        
+
+
+
+
+
+
+
+
+
+
+      }
+
+
+
+
+
+
+
+      
+
+
+
+  });
+  
